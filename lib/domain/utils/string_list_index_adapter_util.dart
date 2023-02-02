18bca5a7862d3569be35dc86a3e74b03f70d99d5
daniel.guimaraes <daniel.guimaraes@crptecnologia.com.br>
2023-02-02 17:54:22 -0300

    fix(domain): Corrige identificação de elemento na expressão para identificação apenas de operandos

--- lib/domain/utils/string_list_index_adapter_util.dart
+++ lib/domain/utils/string_list_index_adapter_util.dart
@@ -21,17 +21,37 @@ mixin StringListIndexAdapterUtil {
     required String expressionString,
     required List<String> expression,
   }) {
-    int cursorPos = offset > 0 ? offset - 1 : offset;
+    if (offset > expressionString.length) return -1;
 
-    String preffix = expressionString.substring(0, cursorPos);
+    int selection = offset > 0 ? offset - 1 : offset;
+
+    String preffix = expressionString.substring(0, offset);
     int countSpaces =
         preffix.isNotEmpty ? RegExp(r' ').allMatches(preffix).length : 0;
 
-    int queryIndex = countSpaces > 0 ? cursorPos - countSpaces : cursorPos;
+    int queryIndex = countSpaces > 0 ? selection - countSpaces : selection;
 
-    List<Set<int>> interface = _buildIndexInterface(expression);
-
-    return interface
+    int index = _buildIndexInterface(expression)
         .indexWhere((path) => queryIndex.isBetween(path.first, path.last));
+
+    if (index <= 0 || num.tryParse(expression[index]) != null) return index;
+
+    int preffixUntilNumber = 0;
+    for (var char in preffix.split('').reversed) {
+      if (num.tryParse(char) != null) break;
+      preffixUntilNumber++;
+    }
+
+    if (preffixUntilNumber == 0) return index - 1;
+
+    String suffix = expressionString.substring(offset);
+
+    int suffixUntilNumber = 0;
+    for (var char in suffix.split('')) {
+      if (num.tryParse(char) != null) break;
+      suffixUntilNumber++;
+    }
+
+    return index + (preffixUntilNumber >= suffixUntilNumber ? 1 : -1);
   }
 }
