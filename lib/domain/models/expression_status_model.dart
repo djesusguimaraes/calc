aa5728d6dfd913816d4b0c9556447900a83c73d9
daniel.guimaraes <daniel.guimaraes@crptecnologia.com.br>
2023-02-03 15:39:33 -0300

    feat: Adiciona validação de expressão pós-fixa para expression_status_model

--- lib/domain/models/expression_status_model.dart
+++ lib/domain/models/expression_status_model.dart
@@ -2,14 +2,27 @@ import '../enums/operand_type_enum.dart';
 
 class ExpressionStatus {
   final List<String> postfixExpression;
-  final OperandType? lastOperandType;
   final int openParenthesisCount;
+  late final bool isValid;
 
   ExpressionStatus({
-    this.lastOperandType,
-    this.postfixExpression = const [''],
+    required this.postfixExpression,
     this.openParenthesisCount = 0,
-  });
+  }) {
+    assert(postfixExpression.isNotEmpty);
+
+    int numberCount = 0;
+    int operatorCount = 0;
+    for (var element in postfixExpression) {
+      OperandType.fromString(element).isOperator
+          ? operatorCount++
+          : numberCount++;
+    }
+
+    isValid = !hasOpenParenthesis &&
+        operatorCount < numberCount &&
+        OperandType.fromString(postfixExpression.last).isOperator;
+  }
 
   bool get hasOpenParenthesis => openParenthesisCount > 0;
 }
