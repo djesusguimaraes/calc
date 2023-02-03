dcc4cf74cb08b1dbf537aa6c4d8a371a9ca955d9
daniel.guimaraes <daniel.guimaraes@crptecnologia.com.br>
2023-02-03 11:34:23 -0300

    fix: Corrige valoração da distância entre o index encontrado e o número mais próximo em `_charsUntilNumber`

--- lib/domain/utils/string_list_index_adapter_util.dart
+++ lib/domain/utils/string_list_index_adapter_util.dart
@@ -1,21 +1,7 @@
 import 'package:ui_plays/domain/extensions/range_extension.dart';
 
 mixin StringListIndexAdapterUtil {
-  List<Set<int>> _buildIndexInterface(List<String> expression) {
-    var interface = <Set<int>>[];
-    int likeStringIndex = 0;
-    for (var element in expression) {
-      var indexes = {likeStringIndex};
-      if (element.length > 1) {
-        likeStringIndex += element.length - 1;
-        indexes.add(likeStringIndex);
-      }
-      interface.add(indexes);
-      likeStringIndex++;
-    }
-    return interface;
-  }
-
+  /// Returns the index of an operand in the list that is closest to the cursor
   int foundItemIndexByCursorPos({
     required int offset,
     required String expressionString,
@@ -31,27 +17,61 @@ mixin StringListIndexAdapterUtil {
 
     int queryIndex = countSpaces > 0 ? selection - countSpaces : selection;
 
-    int index = _buildIndexInterface(expression)
-        .indexWhere((path) => queryIndex.isBetween(path.first, path.last));
+    int index = _buildIndexInterface(expression).indexWhere(
+      (path) => queryIndex.isBetween(path.first, path.last),
+    );
 
     if (index <= 0 || num.tryParse(expression[index]) != null) return index;
 
-    int preffixUntilNumber = 0;
-    for (var char in preffix.split('').reversed) {
-      if (num.tryParse(char) != null) break;
-      preffixUntilNumber++;
+    return _moveToClosestNumber(
+        preffix, expressionString.substring(offset), index);
+  }
+
+  /// Returns an array that represents the indexes limit of each item in the list
+  /// based on the string indexes of the expression
+  ///
+  /// * Example:
+  ///   * `expression`: `['123', '+', '258', '*', '3']`
+  ///   * `interface`: `[{0, 2}, {3}, {4, 6}, {7}, {8}]`
+  List<Set<int>> _buildIndexInterface(List<String> expression) {
+    var interface = <Set<int>>[];
+    int likeStringIndex = 0;
+    for (var element in expression) {
+      var indexes = {likeStringIndex};
+      if (element.length > 1) {
+        likeStringIndex += element.length - 1;
+        indexes.add(likeStringIndex);
+      }
+      interface.add(indexes);
+      likeStringIndex++;
     }
+    return interface;
+  }
 
-    if (preffixUntilNumber == 0) return index - 1;
+  _moveToClosestNumber(
+    String preffix,
+    String suffix,
+    int index,
+  ) {
+    int prefCount = _charsUntilNumber(preffix, reverse: true);
 
-    String suffix = expressionString.substring(offset);
+    if (prefCount == 1) return index - 1;
 
-    int suffixUntilNumber = 0;
-    for (var char in suffix.split('')) {
+    int suffCount = _charsUntilNumber(suffix);
+
+    if (suffCount == 1) return index + 1;
+
+    return index + (prefCount >= suffCount ? suffCount : -prefCount);
+  }
+
+  _charsUntilNumber(String value, {bool reverse = false}) {
+    int count = 1;
+    List<String> iterable = value.trim().split(' ');
+    if (reverse) iterable = iterable.reversed.toList();
+    for (var char in iterable) {
       if (num.tryParse(char) != null) break;
-      suffixUntilNumber++;
+      count++;
     }
-
-    return index + (preffixUntilNumber >= suffixUntilNumber ? 1 : -1);
+    return count;
   }
 }
